122c8cc06e0653b5363e94a02e5c63c0bedc5f84
alexandr <alexandrov6927@gmail.com>
2019-07-26 23:53:07 +0300

    Updates

---
+++ precache-manifest.2d2d60a1705da9a41806fb45e0752d2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87a98fcb4ca330b917e9ce39ba8a1b75",
+    "url": "/react_todo-app/index.html"
+  },
+  {
+    "revision": "71673f363dd390d71e16",
+    "url": "/react_todo-app/static/css/main.fba19be6.chunk.css"
+  },
+  {
+    "revision": "1c396f2594295a30d429",
+    "url": "/react_todo-app/static/js/2.09829138.chunk.js"
+  },
+  {
+    "revision": "71673f363dd390d71e16",
+    "url": "/react_todo-app/static/js/main.37aed12d.chunk.js"
+  },
+  {
+    "revision": "51af3b3c9c5f8ccb1c53",
+    "url": "/react_todo-app/static/js/runtime~main.df364381.js"
+  }
+]);
